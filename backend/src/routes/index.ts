594f69e43e790a81d9ce20eac82ef0ddd81742a6
jary1802 <jaryjary180205@gmail.com>
2026-03-16 17:43:06 +0700

    confirm webhook

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -198,8 +198,8 @@ router.delete('/cart', authenticate, cartController.clearCart);
 //payment online
 // router.post("/create-payment", createPaymentLink);
 // router.post("/webhook", payosWebhook);
-// router.post("/confirm-webhook", confirmWebhook);
-// router.delete("/orders/:id/cancel-payment", cancelPaymentOrder);
+router.post("/confirm-webhook", confirmWebhook);
+router.delete("/orders/:id/cancel-payment", cancelPaymentOrder);
 
 
 
